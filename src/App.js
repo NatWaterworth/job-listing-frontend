a806ec492f08de26ee85c23e4c08d4f6b8524f09
Nat Waterworth <nat.waterworthmindtech.global>
2025-04-01 18:02:43 +0100

    Pages Scaffolding 2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 ﻿import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import JobList from './pages/JobList';
+import JobDetails from './pages/JobDetails';
+import CreateJob from './pages/CreateJob';
+import EditJob from './pages/EditJob';
+import NotFound from './pages/NotFound';
 
-function App() {
+export default function App() {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/jobs" element={<JobList />} />
+                <Route path="/jobs/:id" element={<JobDetails />} />
+                <Route path="/create" element={<CreateJob />} />
+                <Route path="/edit/:id" element={<EditJob />} />
+                <Route path="*" element={<NotFound />} />
             </Routes>
         </Router>
     );
 }
-
-export default App;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,3 @@
 export default function Home() {
-    return (
-        <div className="flex justify-center items-center min-h-screen bg-blue-100">
-            <h1 className="text-4xl font-bold text-blue-800">Welcome to Job Portal</h1>
-        </div>
-    );
+    return <h1>Home Page</h1>;
 }
