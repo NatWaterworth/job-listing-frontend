572090a2df663640b42fb29e4685843d739f24e9
Nat Waterworth <nat.waterworthmindtech.global>
2025-04-10 13:50:20 +0100

    Cleanup Unused Tabs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 ﻿import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import HomePage from './pages/HomePage';
 import JobListPage from './pages/JobListPage';
 import JobDetailPage from './pages/JobDetailPage';
-import CreateJobPage from './pages/CreateJobPage';
-import EditJobPage from './pages/EditJobPage';
 import NotFoundPage from './pages/NotFoundPage';
 import ApplicationPage from "./pages/ApplicationPage";
 import MyJobsPage from "./pages/MyJobsPage";
@@ -14,12 +11,9 @@ export default function App() {
         <Router>
             <Navbar />
             <Routes>
-                <Route path="/" element={<HomePage />} />
                 <Route path="/jobs" element={<JobListPage />} />
                 <Route path="/jobs/:id" element={<JobDetailPage />} />
                 <Route path="/my-jobs" element={<MyJobsPage />} />
-                <Route path="/create" element={<CreateJobPage />} />
-                <Route path="/edit/:id" element={<EditJobPage />} />
                 <Route path="/apply" element={<ApplicationPage />} />
                 <Route path="*" element={<NotFoundPage />} />
             </Routes>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,6 @@ export default function Navbar() {
       <div className="container mx-auto flex justify-between items-center">
         <Link to="/" className="font-bold text-xl">JobFinder</Link>
         <div className="space-x-10">
-          <Link to="/" className="hover:underline">Home</Link>
           <Link to="/jobs" className="hover:underline">Jobs</Link>
           <Link to="/my-jobs" className="hover:underline">My Jobs</Link>
         </div>
